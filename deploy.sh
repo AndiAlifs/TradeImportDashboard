7790f3f021407274a5c81224999f167313c77a5c
Andi Alifsyah <andyalyfsyah4@gmail.com>
2026-03-25 10:40:14 +0700

    fix: Remove trailing slash from proxy_pass in Nginx configuration

--- deploy.sh
+++ deploy.sh
@@ -149,7 +149,7 @@ server {
     }
 
     location /api/ {
-        proxy_pass http://127.0.0.1:${APP_PORT}/;
+      proxy_pass http://127.0.0.1:${APP_PORT};
         proxy_http_version 1.1;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
@@ -189,7 +189,7 @@ server {
     }
 
     location /api/ {
-        proxy_pass http://127.0.0.1:${APP_PORT}/;
+      proxy_pass http://127.0.0.1:${APP_PORT};
         proxy_http_version 1.1;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
